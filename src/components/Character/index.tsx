f7bc8fcd95b665f85e7fdc8d9a1321f71ab86adf
manoj2005-git <sasanapurimanoj@gmail.com>
2026-08-19 20:16:20 +0530

    Replace 3D avatar with supplied portrait

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,7 +1,25 @@
-import Scene from "./Scene";
+import { useEffect } from "react";
+import { useLoading } from "../../context/LoadingProvider";
 
 const CharacterModel = () => {
-  return <Scene />;
+  const { setLoading } = useLoading();
+
+  useEffect(() => {
+    setLoading(100);
+  }, [setLoading]);
+
+  return (
+    <div className="character-container">
+      <div className="character-model">
+        <img
+          className="avatar-image"
+          src="/images/avatar.webp"
+          alt="3D illustrated portrait of Manoj"
+          onLoad={() => setLoading(100)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CharacterModel;
