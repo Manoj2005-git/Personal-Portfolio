4bda32f43538df5a4208550765e60ced7bce81d8
manoj2005-git <sasanapurimanoj@gmail.com>
2026-08-19 19:55:46 +0530

    Fix avatar head framing

--- src/components/Character/utils/character.ts
+++ src/components/Character/utils/character.ts
@@ -46,8 +46,8 @@ export function createRikinCharacter(
           }
         }
       });
-      model.scale.setScalar(4.8);
-      model.position.set(0, -0.9, 0);
+      model.scale.setScalar(3.8);
+      model.position.set(0, -0.8, 0);
       character.add(model);
       dracoLoader.dispose();
       onLoaded?.();
