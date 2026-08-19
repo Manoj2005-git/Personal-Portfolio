a625c9ae5af5af61125b2936f195d6cd77e58eb4
manoj2005-git <sasanapurimanoj@gmail.com>
2026-08-19 20:18:02 +0530

    Restore original 3D avatar

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,25 +1,7 @@
-import { useEffect } from "react";
-import { useLoading } from "../../context/LoadingProvider";
+import Scene from "./Scene";
 
 const CharacterModel = () => {
-  const { setLoading } = useLoading();
-
-  useEffect(() => {
-    setLoading(100);
-  }, [setLoading]);
-
-  return (
-    <div className="character-container">
-      <div className="character-model">
-        <img
-          className="avatar-image"
-          src="/images/avatar.webp"
-          alt="3D illustrated portrait of Manoj"
-          onLoad={() => setLoading(100)}
-        />
-      </div>
-    </div>
-  );
+  return <Scene />;
 };
 
 export default CharacterModel;
